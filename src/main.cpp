6e3fc3dc9f85de4843398f412305ae495089e9fe
julia <j.gazolla@grad.ufsc.br>
2025-09-07 18:49:15 -0300

    add prints to test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include "gateway.h"
 
+#include "utils.h"
+#include <cstdio>
+
 int main(int argc, char** argv) {
+
+    uint64_t time = get_microseconds_now();
+
+    std::cout << "!!!!!!!!!!! vm rodando programa principal !!!!!!! " << time << "\n"; 
+
     Gateway g;
     g.start();
 }
