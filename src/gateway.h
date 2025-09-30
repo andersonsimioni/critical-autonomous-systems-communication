fc33dbafc0b83f83ee93e796bc402ca13d921c28
Anderson <anderson.simioni22@gmail.com>
2025-09-30 19:51:33 -0300

    sem_sync

--- src/gateway.h
+++ src/gateway.h
@@ -17,6 +17,8 @@ public:
     {
         printf("[CAR COMPONENT][%s] initializing..\n", this->name().c_str());
 
+        sem_init(this->sem_sync, 0, 0);
+
         // Initialize communicators and protocol
         this->initialize_communicator(is_master_node, total_vms);
 
@@ -24,23 +26,8 @@ public:
         this->_protocol->enable_sync(total_vms, this->_local);
 
         // Wait for GO message via Communicator queue
-        bool synced = false;
-        while (!synced) {
-            Rx rx = this->_comm->receive(); // Blocking
-            std::string payload(rx.payload.begin(), rx.payload.end());
-
-            // Remove ID header if present
-            if (payload.rfind("ID=", 0) == 0) {
-                size_t space_pos = payload.find(' ');
-                if (space_pos != std::string::npos)
-                    payload = payload.substr(space_pos + 1);
-            }
-
-            if (payload == "GO") {
-                printf("[SYNC] Received GO. All VMs synced. Starting ticks.\n");
-                synced = true;
-            }
-        }
+        sem_wait(&this->sem_sync)
+        printf("[SYNC] Received GO. All VMs synced. Starting ticks.\n");
 
         printf("[CAR COMPONENT][%s] initialized! Ready to start\n", this->name().c_str());
     }
@@ -48,6 +35,19 @@ public:
 protected:
     bool wants_tick() override { return true; }
     unsigned tick_period_ms() override { return 1000; }
+
+    virtual void on_receive(const typename CommunicatorT::Rx& rx) 
+    {
+        std::string payload(rx.payload.begin(), rx.payload.end());
+
+        // Remove ID header if present
+        if (payload.rfind("ID=", 0) == 0) {
+            size_t space_pos = payload.find(' ');
+            if (space_pos != std::string::npos) payload = payload.substr(space_pos + 1);
+        }
+
+        if (payload == "GO") sem_post(sem_sync);
+    }
 
     void on_tick() override {
         // Build payload
@@ -58,4 +58,5 @@ protected:
     }
 
 private:
+    sem_t sem_sync;
 };
